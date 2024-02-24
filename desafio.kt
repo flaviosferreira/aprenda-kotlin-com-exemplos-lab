e7cad147e1768053eac08cd46723a62ad6b0e5c1
Flávio Santos <142853102+flaviosferreira@users.noreply.github.com>
2024-02-24 20:58:06 -0300

    Ajuste na programação

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,50 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val email: String, val nivelExperiencia: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val descricao: String = "")
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
     val inscritos = mutableListOf<Usuario>()
-    
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
+
+    fun removerConteudo(conteudo: ConteudoEducacional) {
+        conteudos.remove(conteudo)
+    }
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+//TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na $nome.")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+  //  TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+  //  TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 45, "Fundamentos básicos.")
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 60, "Princípios de OOP.")
+
+    val formacaoKotlin = Formacao("Kotlin")
+    formacaoKotlin.adicionarConteudo(conteudo1)
+    formacaoKotlin.adicionarConteudo(conteudo2)
+
+    val usuario1 = Usuario("Flávio", "flavio@email.com", Nivel.BASICO)
+    val usuario2 = Usuario("Samara", "samara@email.com", Nivel.INTERMEDIARIO)
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+
+    formacaoKotlin.removerConteudo(conteudo1)
+
+ 
+    println("Inscritos na formação ${formacaoKotlin.nome}: ${formacaoKotlin.inscritos.map { it.nome }}")
 }
